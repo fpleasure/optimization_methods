297631eed908d90ad3b350286d1c3bb62bc4c5fa
Nikita Korolev <niki.korolev@gmail.com>
2025-02-10 22:42:45 +0300

    EDIT: совместил два тестовых примера в один

--- examples/multidimensional_and_plot.py
+++ examples/multidimensional_and_plot.py
@@ -8,5 +8,6 @@ def f(x):
 	return x[0] ** 2 + x[1] ** 2
 
 result = optimizer.minimize(f, np.array([-15.0, 35.0]), tolerance=1e-2)
+print(result)
 plot = OptimizationPlotter(result, f).plot()
 plot.show()
